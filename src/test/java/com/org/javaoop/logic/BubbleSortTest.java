5eeb678472469d87d587de8b2fc781693ad05cd9
codongmin <codongmin@gmail.com>
2024-03-13 01:23:21 +0900

    Implement bubble sort
    버블 소트 구현과 테스트
    
    -

---
+++ src/test/java/com/org/javaoop/logic/BubbleSortTest.java
@@ -0,0 +1,24 @@
+package com.org.javaoop.logic;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @DisplayName("버블 소트 - 리스트를 넣으면 정렬된 결과를 리턴")
+    @Test
+    void given_List_WhenExcuting_ThenReturnSortedList() {
+        // given
+        BubbleSort<Integer> bubbleSort = new BubbleSort<>();
+
+        // when
+        List<Integer> actual = bubbleSort.sort(List.of(3, 1, 2, 4, 5));
+
+        // then
+        assertEquals(List.of(1, 2, 3, 4, 5), actual);
+    }
+}
