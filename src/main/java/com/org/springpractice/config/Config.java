8c251b363099ac177747df4943121facdc9da4af
codongmin <codongmin@gmail.com>
2024-03-16 22:55:29 +0900

    Config #5-spring-configuration
    스프링 설정, 루트 패키지 컴포넌트 스캔

---
+++ src/main/java/com/org/springpractice/config/Config.java
@@ -0,0 +1,10 @@
+package com.org.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.org.springpractice")
+@Configuration
+public class Config {
+
+}
